efd9c2a48cf5c58fb862eed8c0fa834142fb7238
Jorrit Breeuwer <codegorilla@localhost.localdomain>
2018-03-15 12:09:06 +0100

    Merge conflicts resolved

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,24 +9,6 @@
 
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <link href="css/themissinglinktest.css" rel="stylesheet" type="text/css">
-
-<<<<<<< HEAD
-=======
-        <!-- Styles 
-        /* AIzaSyAnmPaoxfofvPJ3z9W8tXQeO0J4EJezejU
-        inloggegevens API-sleutel: AIzaSyAznzmRGq4L6dzx-TSB9d6kv3Bg3x9YS9A naam API-sleutel 1*/
-        <script async defer src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap" type="text/javascript"></script>
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
->>>>>>> f7706b12e0ef828d20aaddab6df394048992e2ff
-
     </head>
     <body>
 
@@ -41,30 +23,5 @@
 <a href="#" class="myButton button5"><img src="img/images.jpg" class="btnimage zorg"><p>Zorg</p></a>
 <a href="#" class="myButton button6"><img src="img/images.jpg" class="btnimage gezelschap"><p>Gezelschap</p></a>
 </div>
-
-
-<<<<<<< HEAD
-=======
-            <div class="content">
-<div class="header">
-    <h2 class="header1">The Missing Link</h2>
-</div>
-<br /><br />
-<div class="container1">
-<button class="button button5"><span class="area">Nood</span></button>
-<button class="button button5"><span class="area">Hulp</span></button>
-<button class="button button5"><span class="area">Zorg</span></button>
-<div><br />
-<div class="container2">
-<button class="button button5"><span class="area">Gezelschap</span></button>
-<button class="button button5"><span class="area">Klussen</span></button>
-<button class="button button5"><span class="area">Boodschappen</span></button>
-</div>
-<div class="footer">
-<h2 class="header1">Always Under Construction!</h2>
-</div>
->>>>>>> f7706b12e0ef828d20aaddab6df394048992e2ff
-
-
     </body>
 </html>
