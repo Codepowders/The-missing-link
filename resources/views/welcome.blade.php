da982e6cd5dc0020556c8fec0239d051b792f8a8
Lieuwe Jelle van der Meulen <l.j.van.der.meulen@gmail.com>
2018-03-16 13:38:07 +0100

    welkom edit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,30 +10,24 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <link href="css/themissinglinktest.css" rel="stylesheet" type="text/css">
 
-<<<<<<< HEAD
-=======
-        <!-- Styles 
-        /* AIzaSyAnmPaoxfofvPJ3z9W8tXQeO0J4EJezejU
-        inloggegevens API-sleutel: AIzaSyAznzmRGq4L6dzx-TSB9d6kv3Bg3x9YS9A naam API-sleutel 1*/
-        <script async defer src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap" type="text/javascript"></script>
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
->>>>>>> f7706b12e0ef828d20aaddab6df394048992e2ff
 
     </head>
     <body>
-
+    <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('register') }}">Register</a>
+                    @endauth
+                </div>
+            @endif
+            <br /><br />
 
 
 <div class="container">
-
 <a href="#" class="myButton button1"><img src="img/images.jpg" class="btnimage nood"><p>Nood</p></a>
 <a href="#" class="myButton button2"><img src="img/images.jpg" class="btnimage boodschappen"><p>Boodschappen</p></a>
 <a href="#" class="myButton button3"><img src="img/images.jpg" class="btnimage klusjes"><p>Klusjes</p></a>
@@ -43,28 +37,7 @@
 </div>
 
 
-<<<<<<< HEAD
-=======
-            <div class="content">
-<div class="header">
-    <h2 class="header1">The Missing Link</h2>
-</div>
-<br /><br />
-<div class="container1">
-<button class="button button5"><span class="area">Nood</span></button>
-<button class="button button5"><span class="area">Hulp</span></button>
-<button class="button button5"><span class="area">Zorg</span></button>
-<div><br />
-<div class="container2">
-<button class="button button5"><span class="area">Gezelschap</span></button>
-<button class="button button5"><span class="area">Klussen</span></button>
-<button class="button button5"><span class="area">Boodschappen</span></button>
-</div>
-<div class="footer">
-<h2 class="header1">Always Under Construction!</h2>
-</div>
->>>>>>> f7706b12e0ef828d20aaddab6df394048992e2ff
 
-
+        </div>
     </body>
 </html>
