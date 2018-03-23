fe8a33d6ad8986f1ad0211de100c0b059ade90b5
Lieuwe Jelle van der Meulen <l.j.van.der.meulen@gmail.com>
2018-03-23 14:59:32 +0100

    new stuff

--- resources/views/seniors/senior.blade.php
+++ resources/views/seniors/senior.blade.php
@@ -2,27 +2,49 @@
 
 @section('content')
 <style>
-.h2 {
-     display: block;
-    font-size: 1.5em;
-    margin-top: 0.83em;
-    margin-bottom: 0.83em;
-    margin-left: 0;
-    margin-right: 0;
-    font-weight: bold;
-}
+.namefield {
+	  font-size: 12pt;
+    float:left;
+    width:250px; }
+#fieldspace {
+    width:250px;
+    height:2px;	}
 </style>
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-            <br /><br /><div class="h2">Login scherm</div><br />
+            <br /><br /><div class="h2">Senior scherm</div><br />
 
                 <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                    <form class="form-horizontal" method="POST" action="{{ route('senior') }}">
                         {{ csrf_field() }}
+                       
+<span class='namefield'>Senior naam</span>
+<input type='text' size='46' name='klantnaam' value='$row[klantnaam]' />
+<div id='fieldspace'></div>
+<span class='namefield'>Achternaam</span>
+<input type='text' size='46' maxlength='40' name='achternaam' value='$row[achternaam]' />
+<div id='fieldspace'></div>
+<span class='namefield'>Adres</span>
+<input type='text' size='46' maxlength='40' name='adres' value='$row[adres]' />
+<div id='fieldspace'></div>
+<span class='namefield'>Postcode</span>
+<input type='text' size='46' maxlength='7' name='postcode' value='$row[postcode]' />
+<div id='fieldspace'></div>
+<span class='namefield'>Woonplaats</span>
+<input type='text' size='46' maxlength='40' name='woonplaats' value='$row[woonplaats]' />
+<div id='fieldspace'></div>
+<span class='namefield'>Telefoonnummer</span>
+<input type='text' size='46' maxlength='10' name='telefoon' value='$row[telefoonnummer]' />
+<div id='fieldspace'></div><div id='fieldspace'></div>   
+<div id='fieldspace'></div><div id='fieldspace'></div>
+_END;
+		echo "<a href='#' class='button'>Senior aanmaken</a>";
+		echo '<button type="button" class="button" onclick="sluiten();">Sluiten</button></form></div>';
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+
+ <!--                       <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-6">
@@ -70,7 +92,7 @@
                                     Forgot Your Password?
                                 </a>
                             </div>
-                        </div>
+                        </div> -->
                     </form>
                 </div>
             </div>
