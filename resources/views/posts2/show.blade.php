39c2c9c6a84bf3c4e3ac9a1e21d7fc01b2efe232
Jorrit Breeuwer <codegorilla@localhost.localdomain>
2018-04-09 15:52:25 +0200

    small changes

--- resources/views/posts2/show.blade.php
+++ resources/views/posts2/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <a href="/posts2" class="btn btn-default">Go Back</a>
     <h1>{{!!$post->title!!}}</h1>
-    <img style="width:100%" src="/storage/cover_images/{{$post->cover_image}}">
+    <img style="width:100%" src="../storage/cover_images/{{$post->cover_image}}">
     <br><br>
     <div>
         {!!$post->body!!}
@@ -13,7 +13,7 @@
     <hr>
     @if(!Auth::guest())
         @if(Auth::user()->id == $post->user_id)
-            <a href="/posts2/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+            <a href="./posts2/{{$post->id}}/edit" class="btn btn-default">Edit</a>
 
             {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
                 {{Form::hidden('_method', 'DELETE')}}
